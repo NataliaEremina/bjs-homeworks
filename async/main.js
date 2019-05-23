28f8fd5bd166d3be3a8fe5b20dd1ab8ed603c2d8
NataliaEremina <ENA-100@yandex.ru>
2019-05-23 23:20:41 +0600

    changes in task 1

--- async/main.js
+++ async/main.js
@@ -7,19 +7,26 @@ function setDailyRhythm(wakeUpTime, bedTime) {
     let bed = () => alert('Спокойной ночи, Вася!');
     let goodnight = setAlarm(bedTime, bed);
     
-    setInterval (setAlarm, 1000);   
+    setInterval (goodmorning, 1000);
+    setInterval (goodnight, 1000);   
 }
     
 
 function setAlarm(time, callback) {
     let clock = new Date;
-    let hours= clock.getHours();
-    let mins= clock.getMinutes();
-    let datastr = hours+":"+ mins;
-    if (datastr == time) {
+    let hours= clock.getHours().toString;
+    let mins= clock.getMinutes().toString;
+    let datastr = hours+ ":" + mins;
+    if (hours <10) {
+        hours = "0" + hours;
+    }
+    if (mins <10) {
+        mins = "0" + mins;
+    }
+    if (datastr === time){
         return callback;
     } 
- 
-}
+    
+    }
 
 
